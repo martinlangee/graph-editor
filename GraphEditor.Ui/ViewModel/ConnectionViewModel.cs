01307e2679eee06ce2880bb8f7194ba974d4ccf9
martin <martin.lange@csa-germany.de>
2019-02-19 22:48:30 +0100

    ConnectionViewModel class added

---
+++ GraphEditor.Ui/ViewModel/ConnectionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace GraphEditor.ViewModel
+{
+    public class ConnectionViewModel: BaseNotification
+    {
+        bool _isSelected;
+
+        public ConnectionViewModel()
+        {
+            Path = new ObservableCollection<Point>();
+        }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { SetProperty(ref _isSelected, value, nameof(IsSelected)); }
+        }
+
+        public ObservableCollection<Point> Path { get; }
+    }
+}
