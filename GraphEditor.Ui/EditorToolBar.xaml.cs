088cd50e530c0de58d7fc87f7f397bb6823b0730
martin <martin.lange@csa-germany.de>
2019-04-23 12:16:06 +0200

    added solution wide licence header template

--- GraphEditor.Ui/EditorToolBar.xaml.cs
+++ GraphEditor.Ui/EditorToolBar.xaml.cs
@@ -1,4 +1,14 @@
-﻿using GraphEditor.Interface.Nodes;
+﻿#region copyright
+// Initial developer of the original code is Martin Lange.
+//
+// The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at https://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations under the License.
+#endregion
+
+using GraphEditor.Interface.Nodes;
 using GraphEditor.Ui.Tools;
 using System.Windows;
 using System.Windows.Controls;
